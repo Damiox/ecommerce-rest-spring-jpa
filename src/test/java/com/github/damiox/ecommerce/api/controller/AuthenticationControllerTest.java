c061454b886a75f3edaf1001c556c88834a9b71f
Damian Nardelli <damiox@gmail.com>
2018-05-01 06:19:11 -0300

    added unit test for /login API endpoint

---
+++ src/test/java/com/github/damiox/ecommerce/api/controller/AuthenticationControllerTest.java
@@ -0,0 +1,91 @@
+package com.github.damiox.ecommerce.api.controller;
+
+import com.github.damiox.ecommerce.service.SecurityService;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+/**
+ * Unit tests for {@link AuthenticationController}
+ */
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = AuthenticationController.class)
+public class AuthenticationControllerTest {
+
+    private static final String URL = "/login";
+
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private UserDetailsService userDetailsService;
+    @MockBean
+    private SecurityService securityService;
+
+    @Test
+    public void testLogin() throws Exception {
+        Mockito.when(securityService.authenticate(Mockito.eq("user"), Mockito.eq("user"))).thenReturn("secret");
+
+        String request = "{ \"username\": \"user\", \"password\": \"user\" }";
+        mockMvc.perform(MockMvcRequestBuilders.post(URL).content(request).contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.token", Matchers.is("secret")));
+    }
+
+    @Test
+    public void testInvalidUsername() throws Exception {
+        Mockito.when(securityService.authenticate(Mockito.eq("user"), Mockito.eq("user"))).thenReturn("secret");
+
+        String request;
+
+        request = "{ \"username\": null, \"password\": \"user\" }";
+        mockMvc.perform(MockMvcRequestBuilders.post(URL).content(request).contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(MockMvcResultMatchers.status().isBadRequest());
+
+        request = "{ \"username\": \"\", \"password\": \"user\" }";
+        mockMvc.perform(MockMvcRequestBuilders.post(URL).content(request).contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(MockMvcResultMatchers.status().isBadRequest());
+
+        request = "{ \"username\": \"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\", \"password\": \"user\" }";
+        mockMvc.perform(MockMvcRequestBuilders.post(URL).content(request).contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    public void testInvalidPassword() throws Exception {
+        Mockito.when(securityService.authenticate(Mockito.eq("user"), Mockito.eq("user"))).thenReturn("secret");
+
+        String request;
+
+        request = "{ \"username\": \"user\", \"password\": null }";
+        mockMvc.perform(MockMvcRequestBuilders.post(URL).content(request).contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(MockMvcResultMatchers.status().isBadRequest());
+
+        request = "{ \"username\": \"user\", \"password\": \"\" }";
+        mockMvc.perform(MockMvcRequestBuilders.post(URL).content(request).contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(MockMvcResultMatchers.status().isBadRequest());
+
+        request = "{ \"username\": \"user\", \"password\": \"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\" }";
+        mockMvc.perform(MockMvcRequestBuilders.post(URL).content(request).contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+}
